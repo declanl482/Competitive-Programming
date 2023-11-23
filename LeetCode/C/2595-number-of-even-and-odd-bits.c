c38e0f1a47e263e19f0c7bdfb0a2b92097f40564
declanl482 <declan.lin@stonybrook.edu>
2023-11-23 12:58:42 -0500

    added number of even and odd bits solution

---
+++ LeetCode/C/2595-number-of-even-and-odd-bits.c
@@ -0,0 +1,22 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int* evenOddBit(int n, int* returnSize) {
+    *returnSize = 2;
+    int *result = malloc(2 * sizeof(int));    
+    result[0] = 0;
+    result[1] = 0;
+    int even = 0;
+    int odd = 0;
+    for (int i = 0; n != 0; n >>= 1, i++) {
+        if (n & 1) {
+            (i % 2 == 0) ? even++ : odd++;
+        }
+    }
+    result[0] = even;
+    result[1] = odd;
+    return result;
+}
