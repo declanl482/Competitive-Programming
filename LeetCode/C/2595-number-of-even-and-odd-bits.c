12dccaf003db98c3dd7f62e2e2f96461070f5903
Declan <declanl482@gmail.com>
2024-01-09 20:41:06 -0500

    modified compiler path configuration

--- LeetCode/C/2595-number-of-even-and-odd-bits.c
+++ LeetCode/C/2595-number-of-even-and-odd-bits.c
@@ -3,15 +3,18 @@
  */
 #include <stdlib.h>
 
-int* evenOddBit(int n, int* returnSize) {
+int *evenOddBit(int n, int *returnSize)
+{
     *returnSize = 2;
-    int *result = malloc(2 * sizeof(int));    
+    int *result = malloc(2 * sizeof(int));
     result[0] = 0;
     result[1] = 0;
     int even = 0;
     int odd = 0;
-    for (int i = 0; n != 0; n >>= 1, i++) {
-        if (n & 1) {
+    for (int i = 0; n != 0; n >>= 1, i++)
+    {
+        if (n & 1)
+        {
             (i % 2 == 0) ? even++ : odd++;
         }
     }
